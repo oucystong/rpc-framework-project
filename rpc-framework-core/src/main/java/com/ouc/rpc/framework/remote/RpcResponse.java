b8a0dfcce9b8eb88cab992858fcc976bda8c9909
tongyusheng <buaatys@163.com>
2023-03-05 15:23:13 +0800

    增加RPC响应模型

---
+++ rpc-framework-core/src/main/java/com/ouc/rpc/framework/remote/RpcResponse.java
@@ -0,0 +1,29 @@
+package com.ouc.rpc.framework.remote;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * @Description: RPC响应模型
+ * @Author: Mr.Tong
+ */
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RpcResponse {
+    /**
+     * @Description: RPC请求ID
+     */
+    private String requestId;
+    /**
+     * @Description: RPC请求失败/成功标记
+     */
+    private Boolean isSuccess;
+    /**
+     * @Description: RPC响应结果
+     */
+    private Object result;
+}
---
+++ rpc-framework-core/src/test/java/com/ouc/rpc/framework/remote/RpcResponseTest.java
@@ -0,0 +1,20 @@
+package com.ouc.rpc.framework.remote;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @Description:
+ * @Author: Mr.Tong
+ */
+@Slf4j
+public class RpcResponseTest {
+
+
+    @Test
+    void testSetGet() {
+        RpcResponse rpcResponse = new RpcResponse();
+        rpcResponse.setIsSuccess(false);
+        log.info("rpc response is {}", rpcResponse.getIsSuccess().toString());
+    }
+}
